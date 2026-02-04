b9a229cf961b30e846089df0fbf209078f0fefe6
PovDavin <davin.pov@student.cadt.edu.kh>
2026-02-04 23:17:15 +0700

    fix models name

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,11 @@
 import sequelize from "../config/database.js";
-import User from "./user.js";
-import Food from "./food.js";
-import Ingredient from "./ingredient.js";
-import Category from "./category.js";
-import Comment from "./comment.js";
-import Rating from "./rating.js";
-import Favorite from "./favorite.js";
+import User from "User.js";
+import Food from "Food.js";
+import Ingredient from "Ingredient.js";
+import Category from "Category.js";
+import Comment from "Comment.js";
+import Rating from "Rating.js";
+import Favorite from "Favorite.js";
 
 // Food ↔ Ingredient (many-to-many)
 Food.belongsToMany(Ingredient, { through: "food_ingredients", foreignKey: "food_id" });
