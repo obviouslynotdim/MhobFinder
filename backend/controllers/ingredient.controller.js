9dbe4fa85966a7805d5061cb7872d9ff3d4095ff
Lìlz ♡⚡︎ <liza@Lizas-Laptop.local>
2026-03-04 10:40:12 +0700

    Fix loading ingredient

--- backend/controllers/ingredient.controller.js
+++ backend/controllers/ingredient.controller.js
@@ -1,11 +1,26 @@
 import Ingredient from "../models/Ingredient.js";
-import { IngredientType } from "../models/index.js"; // use centralized export to avoid circular issues
+import { IngredientType } from "../models/index.js"; 
 
 export const getAllIngredients = async (req, res, next) => {
   try {
     const ingredients = await Ingredient.findAll({
       include: [{ model: IngredientType, as: "type" }],
     });
+    res.json(ingredients);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getIngredientsByType = async (req, res, next) => {
+  try {
+    const { typeId } = req.params;   
+
+    const ingredients = await Ingredient.findAll({
+      where: { typeId },           
+      include: [{ model: IngredientType, as: "type" }],
+    });
+
     res.json(ingredients);
   } catch (err) {
     next(err);
